ce0fd5675e122482be55eb94364ffb09d835ce73
fischmar000 <fischmar000@warren.k12.in.us>
2021-09-09 12:29:04 -0400

    U2

--- L1-Methods Without Parameters/src/CircleCalc.java
+++ L1-Methods Without Parameters/src/CircleCalc.java
@@ -1,17 +1,33 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
         //call the area method
-
-        //call the circumference method
-
+       area(Math.PI,Math.pow);
+        //call the circumference method//
+       circumference(radius,circumference);
     }
 
     //write a method to calculate a circle's area
+    public static void area(Math.PI,Math.pow) {
+    DecimalFormat round = new DecimalFormat("#.##");
+    double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius of the circle?"));
 
+    double area = Math.PI * Math.pow(radius, 2);
+
+    JOptionPane.showMessageDialog(null, "The area is " + area);
+    }
 
     //write a method to calculate a circle's circumference
+    public static void circumference() {
 
+    double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius of the circle?"));
+
+    double circumference = 2 * Math.PI * radius;
+
+    JOptionPane.showMessageDialog(null, "The area is " + circumference);
+    }
 }
